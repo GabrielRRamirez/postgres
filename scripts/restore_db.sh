9044493a151cdfd97355cce0b7e0d841507965d4
GabrielRamirezVR <gabriel.ramirez@vrsoft.com.br>
2023-03-31 09:01:32 -0300

    Ajuste restoredb.sh - quebra de linha LF

--- scripts/restore_db.sh
+++ scripts/restore_db.sh
@@ -1,7 +1,5 @@
 echo "restaurando banco de dados V4.1"
 pg_restore -U postgres -d stable_4_1 -v /bkp/database.bkp
-
 echo "restaurando banco de dados V4.2"
 pg_restore -U postgres -d main_4_2 -v /bkp/database.bkp
-
 echo "============ restore finalizado ================"
